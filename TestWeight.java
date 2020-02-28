2ac35e01c72ce29b5eda67b07193c2180d84fe3c
aphrx <amalnnath20@yahoo.ca>
2020-02-28 14:34:34 -0500

    update

--- TestWeight.java
+++ TestWeight.java
@@ -1,4 +1,4 @@
-package weight-sensor;
+package weightsensor;
 
 import WeightSensor.*;
 
--- WeightSensor.java
+++ WeightSensor.java
@@ -1,4 +1,4 @@
-package weight-sensor;
+package weightsensor;
 
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
