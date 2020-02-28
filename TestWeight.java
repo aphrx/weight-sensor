4c170e0e93d2061ac14e065354018a1a52474e7d
aphrx <amalnnath20@yahoo.ca>
2020-02-28 14:37:42 -0500

    update

--- TestWeight.java
+++ TestWeight.java
@@ -1,6 +1,32 @@
-package weightsensor;
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalInput;
+import com.pi4j.io.gpio.RaspiPin;
 
-import WeightSensor;
+public class WeightSensor {
+
+    private final int pin_dout;
+    private final int pin_sck;
+
+    private GpioPinDigitalInput wsPin;
+
+    public WeightSensor() {
+        this.pin_dout = 4;
+        this.pin_sck = 5;
+    }
+
+    public void init() {
+        final GpioController controller = GpioFactory.getInstance();
+        wsPin = controller.provisionDigitalInputPin(RaspiPin.getPinByAddress(pin_dout));
+    }
+
+    public Boolean getReading() {
+        if (wsPin == null)
+            throw new IllegalStateException("Digital input pin not initialized");
+
+        return wsPin.getState().isHigh();
+    }
+}
 
 public class TestWeight {
 	
