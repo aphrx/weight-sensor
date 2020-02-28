4268d7f768669b54cffb05e7fbf0d97548fa9ccd
aphrx <amalnnath20@yahoo.ca>
2020-02-28 14:31:02 -0500

    update

--- TestWeight.java
+++ TestWeight.java
@@ -1,9 +1,9 @@
-import WeightSensor
+import WeightSensor;
 
 public class TestWeight {
 	
 	public static void main(String[] args){
-		final WeightSensor ws = new WeightSensor();
+		WeightSensor ws = new WeightSensor();
 		ws.init();
 		ws.getReading();	
 	}	
