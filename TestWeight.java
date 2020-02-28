360d1a5d0d2d89f0c7a117112af2df8574f4fa83
aphrx <amalnnath20@yahoo.ca>
2020-02-28 15:04:34 -0500

    update

--- TestWeight.java
+++ TestWeight.java
@@ -3,6 +3,7 @@ import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalInput;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.gpio.PinState;
 
 class WeightSensor {
 
